4443d2b144f8799a75d7c2eca9783b4a0eb2daa1
alicanyucel <yucelalican@hotmail.com>
2023-10-16 00:25:30 +0300

    sync applied vs code & github

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,7 @@ const app=express;
 const cors=require("cors");
 app.request(express.json());
 app.use(cors());
+// api request
+
 const port=process.env.port || 5000;
 app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
