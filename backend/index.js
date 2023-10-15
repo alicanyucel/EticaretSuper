c58acf03ff5ac6e55aeea265d25017ac1ab3f225
alicanyucel <yucelalican@hotmail.com>
2023-10-16 00:46:36 +0300

    app crashed ?

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express=require("express");
 const app=express;
 const cors=require("cors");
-app.request(express.json());
+app.use(express.json());
 app.use(cors());
 // api request
-
+app.get("",(req,res)=>{
+    res.json({message:"api successfull"})
+});
 const port=process.env.port || 5000;
 app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
