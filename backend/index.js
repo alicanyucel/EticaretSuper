dae2aa6fe1bb05e255ce3c449a82bdf8158eb4d4
alicanyucel <yucelalican@hotmail.com>
2023-10-16 14:40:10 +0300

    mongo db connection string

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,4 @@ app.get("",(req,res)=>{
 });
 const port=process.env.port || 5000;
 app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
+// connection string mongodb+srv://yucelalican30:<password>@cluster0.6axssxz.mongodb.net/?retryWrites=true&w=majority
