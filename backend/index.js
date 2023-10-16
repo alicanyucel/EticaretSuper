ff881579c5a87cd1dabd069b40037fd1e3f50edd
alicanyucel <yucelalican@hotmail.com>
2023-10-16 16:00:09 +0300

    auth router added.

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ app.get("",(req,res)=>{
     res.json({message:"api successfull"})
 });
 connection();
+const authRouter=require("./routers/auth.router");
+app.use("/api/auth",authRouter);
 const port=process.env.port || 5000;
 app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
 // connection string mongodb+srv://yucelalican30:<password>@cluster0.6axssxz.mongodb.net/?retryWrites=true&w=majority
---
+++ backend/routers/auth.authenticate.js
@@ -0,0 +1,25 @@
+const express=require("express");
+const User=require("../models/user");
+const router=express.Router();
+const{v4:uuidv4}=require("uuid");
+const jwt=require("jsonwebtoken");
+const User = require("../models/user");
+const secretKey="My secret key 1234.";
+const options={
+    expressIn:"1d"
+};
+router.post("/register",async(reg,res)=>{
+    try{
+        const User=new User(req.body);
+        user._id=uuidv4();
+        user.createDate= new Date();
+        await User.save();
+        const token=jwt.sign({},secretKey.options);
+        let model={token:token,user:user};
+        res.json(model);
+    }
+    catch(err)
+    {
+       res.status(500).json({message:error.message});
+    }
+});
