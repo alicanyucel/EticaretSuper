728cda003e2e099f07ba03039d2fbb2a74adc304
alicanyucel <yucelalican@hotmail.com>
2023-10-16 15:00:42 +0300

    db context added

---
+++ backend/database/db.js
@@ -0,0 +1,9 @@
+const mongoose=require("mongoose");
+const url="mongodb+srv://yucelalican30:alican12345.@cluster0.6axssxz.mongodb.net/?retryWrites=true&w=majority";
+const connection=()=>{
+    mongoose.connect(url,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    })
+    .then(()=>console.log("bağlantı sağlam")).catch((err)=>console.log("bağlantı yok" + err.message));
+}
