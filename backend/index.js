f60d22b25027497ab99cf77c3d7732cf5e821313
alicanyucel <yucelalican@hotmail.com>
2023-10-16 15:26:01 +0300

    bağlantı sağlam mongo db bağlantısı kuruldu.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express=require("express");
 const app=express();
 const cors=require("cors");
+const connection  = require("./database/db");
 app.use(express.json());
 app.use(cors());
 // api request
 app.get("",(req,res)=>{
     res.json({message:"api successfull"})
 });
+connection();
 const port=process.env.port || 5000;
 app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
 // connection string mongodb+srv://yucelalican30:<password>@cluster0.6axssxz.mongodb.net/?retryWrites=true&w=majority
