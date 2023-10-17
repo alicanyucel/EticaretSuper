ab0619dabf067dbccb8e1e18459a23ae3be52976
alicanyucel <yucelalican@hotmail.com>
2023-10-17 12:19:31 +0300

    models added.

---
+++ frontend/eticaret/src/app/common/models/request.model.ts
@@ -0,0 +1,8 @@
+export class RequestModel{
+    pageNumber: number = 1;
+    pageSize: number = 10;
+    search: string = "";
+    categoryName: string = "Tümü";
+    categoryId: string = "";
+    priceFilter: string = "0";
+}
