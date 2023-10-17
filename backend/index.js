20c600b39a516a4d4bcd39905ea53597ed6329a4
alicanyucel <yucelalican@hotmail.com>
2023-10-17 12:11:34 +0300

    index.js fiiled.

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,26 @@
-const express=require("express");
-const app=express();
-const cors=require("cors");
-const connection  = require("./database/db");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./database/db");
+const path = require("path");
+
 app.use(express.json());
 app.use(cors());
-// api request
-app.get("",(req,res)=>{
-    res.json({message:"api successfull"})
-});
-const authRouter=require("./routers/auth.router");
-app.use("/api/auth",authRouter);
-const port=process.env.port || 5000;
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const authRouter = require("./routers/auth.router");
+const categoryRouter = require("./routers/category.router");
+const productRouter = require("./routers/product.router");
+const basketRouter = require("./routers/basket.router");
+const orderRouter = require("./routers/order.router");
+
+app.use("/api/auth", authRouter);
+app.use("/api/categories",categoryRouter);
+app.use("/api/products", productRouter);
+app.use("/api/baskets", basketRouter);
+app.use("/api/orders", orderRouter);
+
 connection();
-app.listen(port,()=>console.log("uygulama localhost:5000 portundan ayağa kalktı"));
-// connection string mongodb+srv://yucelalican30:<password>@cluster0.6axssxz.mongodb.net/?retryWrites=true&w=majority
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=> console.log("Uygulama http://localhost:5000 portunda ayağa kalktı!"));
